f713e4b626123515abd2d65cff205bf2b241fc87
hoanganhtrans2 <hoanganhtrans23@gmail.com>
2020-12-09 11:59:11 +0700

    add BUS

---
+++ BUS/BUS_DatTruoc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+using Entity;
+
+namespace BUS
+{
+    public class BUS_DatTruoc
+    {
+        DAL_DatTruoc daldt = new DAL_DatTruoc();
+
+        public bool kiemTraDatTruocTieuDe(string matieude)
+        {
+            return daldt.kiemTraDatTruocTieuDe(matieude);
+        }
+        public bool ganMaDia(string madia, string matieude)
+        {
+            return daldt.ganMaDia(madia, matieude);
+        }
+        public int getMaDatTruoc(string madia)
+        {
+            return daldt.getMaDatTruoc(madia);
+        }
+    }
+}
---
+++ BUS/BUS_TaiKhoan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+using Entity;
+
+namespace BUS
+{
+    public class BUS_TaiKhoan
+    {
+        DAL_TaiKhoan Datatk = new DAL_TaiKhoan();
+        public bool dangNhap(string taikhoan, string matkhau)
+        {
+            return Datatk.dangNhap(taikhoan, matkhau);
+        }
+    }
+}
